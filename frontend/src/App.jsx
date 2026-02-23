11f47af9278e0e150143f501c709dd797d524c32
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-02-23 12:51:31 +0530

    edit app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import HireMe from "./components/HireMe";
+import Footer from "./components/Footer";
+import "./App.css";
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
